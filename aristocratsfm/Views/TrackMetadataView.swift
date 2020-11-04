62d131bf4c34d3bccc36710bdd28fe2158b4450d
Vadim Klimenko <mail@vadimklimenko.com>
2020-11-05 00:22:19 +0200

    Refactor views

--- aristocratsfm/Views/TrackMetadataView.swift
+++ aristocratsfm/Views/TrackMetadataView.swift
@@ -14,69 +14,82 @@ struct TrackMetadataView: View {
     
     var stream: String? = Streams.Main.Name
     
+    var artworkView: some View {
+        let playback = nowPlaying.playback;
+        
+        var fillWrapperColor = colorScheme == .dark ? Design.Primary.Base : Color.white
+        if colorScheme == .dark {
+            if case let .playing(_, artwork) = playback {
+                if (artwork != nil) {
+                    fillWrapperColor = Design.Primary.DarkGray
+                }
+            }
+        }
+        
+        let rectangle = Rectangle()
+            .fill(fillWrapperColor)
+            .padding()
+            .aspectRatio(1.0, contentMode: .fit)
+        
+        return rectangle
+    }
+    
+    var artistText: some View {
+        let playback = nowPlaying.playback;
+        var author = "Аристократи"
+        
+        if case let .playing(track, _) = playback {
+            author = track.artist
+        }
+        
+        return Text(author)
+            .font(Font.system(size: 20, weight: .medium, design: .default))
+            .foregroundColor(colorScheme == .dark ? Color.white : Design.Primary.DarkGray)
+            .padding(.top, 20)
+            .padding(.horizontal)
+            .multilineTextAlignment(.center)
+    }
+    
+    var songText: some View {
+        let playback = nowPlaying.playback;
+        var song = "..."
+        
+        if case let .playing(track, _) = playback {
+            song = track.song
+        }
+        
+        return Text(song)
+            .font(Font.system(size: 17, weight: .light, design: .default))
+            .padding(.top, 11)
+            .padding(.horizontal)
+            .foregroundColor(colorScheme == .dark ? Color.white : Design.Primary.LightGray)
+            .multilineTextAlignment(.center)
+    }
+    
     var body: some View {
         let playback = nowPlaying.playback;
         
         let artworkImageName = colorScheme == .dark ? "AristocratsLogoWhite" : "AristocratsLogoPurple"
         
-        let ArtworkImage = Image(artworkImageName)
+        let artworkImage = Image(artworkImageName)
             .padding()
             .scaleEffect(0.5, anchor: .center)
         
-        let ImageWrapperView = Rectangle()
-            .fill(colorScheme == .dark ? Design.Primary.Base : Color.white)
-            .padding()
-            .aspectRatio(1.0, contentMode: .fit)
-            .cornerRadius(30)
-//          TODO To use or not to use?
-//            .frame(maxWidth: 300, alignment: .center)
+        var artworkImageView: ImageView?
+        if case let .playing(_, artwork) = playback {
+            artworkImageView = ImageView(withURL: artwork?.images.first?.thumbnails.large ?? "")
+        }
         
-        VStack() {
-            switch playback {
-            case .nothing:
-                VStack(alignment: .center) {
-                    ImageWrapperView
-                        .overlay(ArtworkImage);
-                    
-                    Text("Аристократи")
-                        .font(Font.system(size: 20, weight: .medium, design: .default))
-                        .foregroundColor(colorScheme == .dark ? Color.white : Design.Primary.DarkGray)
-                        .padding(.top, 20)
-                        .padding(.horizontal)
-                        .multilineTextAlignment(.center)
-                    
-                    Text("...")
-                        .font(Font.system(size: 17, weight: .light, design: .default))
-                        .padding(.top, 11)
-                        .padding(.horizontal)
-                        .foregroundColor(colorScheme == .dark ? Color.white : Design.Primary.LightGray)
-                        .multilineTextAlignment(.center)
-                }
-                
-            case let .playing(track, artwork):
-                if (artwork != nil) {
-                    ImageWrapperView.overlay(
-                        ImageView(withURL: artwork?.images.first?.thumbnails.large ?? "")
-                    );
-                } else {
-                    ImageWrapperView
-                        .overlay(ArtworkImage);
-                }
-                
-                Text(track.song)
-                    .font(Font.system(size: 20, weight: .medium, design: .default))
-                    .foregroundColor(colorScheme == .dark ? Color.white : Design.Primary.DarkGray)
-                    .padding(.top, 20)
-                    .padding(.horizontal)
-                    .multilineTextAlignment(.center)
-                
-                Text(track.artist)
-                    .font(Font.system(size: 17, weight: .light, design: .default))
-                    .padding(.top, 11)
-                    .padding(.horizontal)
-                    .foregroundColor(colorScheme == .dark ? Color.white : Design.Primary.LightGray)
-                    .multilineTextAlignment(.center)
+        return VStack() {
+            // TODO Optimize it somehow
+            if (artworkImageView != nil) {
+                artworkView.overlay(artworkImageView)
+            } else {
+                artworkView.overlay(artworkImage)
             }
+            
+            artistText
+            songText
         }
     }
 }
